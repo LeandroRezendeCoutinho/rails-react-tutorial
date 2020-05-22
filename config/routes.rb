bd1c321ff75a617d22c66a1b7c97967545dd4ddd
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-05-22 08:30:42 -0300

    add a controller to make a bridge to react

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static#index'
   namespace :v1, default: { format: 'json' } do
     get 'things', to: 'things#index'
   end
