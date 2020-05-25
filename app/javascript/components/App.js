552fe043e49efbc59af07675a1968879f893e5ff
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-05-25 08:47:40 -0300

    routing App with react router
    make rails send all requests that are not for the API to our App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,18 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HelloWorld from "./HelloWorld";
+
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => "Home"} />
+          <Route path="/hello" render={() => <HelloWorld greeting="Fiend" />} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
-export default App
+export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   namespace :v1, default: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+
+  get '*page', to: 'static#index', constraints: -> (req) do
+    !req.xhr? && req.format.html?
+  end
 end
