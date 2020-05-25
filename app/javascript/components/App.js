02b6252d3aded71d715821ca13f0c570aa0954a6
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-05-25 09:10:44 -0300

    setup redux state store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HelloWorld from "./HelloWorld";
+import { configureStore } from "../configureStore.js";
+
+const store = configureStore();
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => "Home"} />
-          <Route path="/hello" render={() => <HelloWorld greeting="Fiend" />} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => "Home"} />
+            <Route
+              path="/hello"
+              render={() => <HelloWorld greeting="Fiend" />}
+            />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
